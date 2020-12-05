7577a1d0db422498c4eae53dbef41e962f3e048d
Ashton Hudson <ashtonhudson1@gmail.com>
2020-12-05 09:15:07 +0200

    Adding day 5

--- day_05/day5.py
+++ day_05/day5.py
@@ -1,3 +1,23 @@
+def parse_boarding_pass(b_pass):
+    row = b_pass[0:7].replace("F", "0").replace("B", "1")
+    seat = b_pass[7:10].replace("R", "1").replace("L", "0")
+    return int(row, 2) * 8 + int(seat, 2)
+
+
+def all_ids(b_passes):
+    return [parse_boarding_pass(i) for i in b_passes]
+
+
+def find_seat(ids):
+    ids.sort()
+    for i, v in enumerate(ids):
+        if i > 0 and (v - ids[i - 1]) == 2:
+            return v - 1
+
+
 if __name__ == "__main__":
     with open("data.txt", "r") as file:
-        data = file.read()
+        data = file.read().split("\n")
+
+    print("Part 1:", max(all_ids(data)))
+    print("Part 2:", find_seat(all_ids(data)))
--- day_05/test.py
+++ day_05/test.py
@@ -1,1 +1,4 @@
-from day5 import *
+from day5 import parse_boarding_pass, all_ids
+
+tests = ["BFFFBBFRRR", "FFFBBBFRRR", "BBFFBBFRLL"]
+assert all_ids(tests) == [567, 119, 820]
