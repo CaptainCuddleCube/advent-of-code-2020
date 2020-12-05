721f5d92efe2c9262076510e6795b7542bf297d5
Ashton Hudson <ashtonhudson1@gmail.com>
2020-12-05 17:09:03 +0200

    Slight refactoring to clean things up a little.

--- day_05/day5.py
+++ day_05/day5.py
@@ -4,16 +4,14 @@ def parse_boarding_pass(b_pass):
     return int(row, 2) * 8 + int(seat, 2)
 
 
-def all_ids(b_passes):
-    return [parse_boarding_pass(i) for i in b_passes]
-
-
 def find_seat(ids):
     ids.sort()
     for i, v in enumerate(ids):
         if i > 0 and (v - ids[i - 1]) == 2:
             return v - 1
 
+
+all_ids = lambda b_passes: [parse_boarding_pass(i) for i in b_passes]
 
 if __name__ == "__main__":
     with open("data.txt", "r") as file:
